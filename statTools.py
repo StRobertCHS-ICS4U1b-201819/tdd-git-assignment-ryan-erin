dd2cd9662ad794a62ecd5f39be56886a39110a18
Rezhe <rzwang.ca@gmail.com>
2018-11-02 23:21:46 -0400

    Changes to be committed: 	modified:   statTools.py 	modified:   test_statTools.py

--- statTools.py
+++ statTools.py
@@ -1,1 +1,12 @@
+import pytest
+from functools import reduce
+
+def mean(anyList):
+    if anyList != []:
+        return reduce(lambda a, b: a + b, anyList) / len(anyList)
+    else:
+        pass
+
+
+
 
--- test_statTools.py
+++ test_statTools.py
@@ -1,10 +1,11 @@
-def all_done():
-    pass
+import pytest
+from statTools import *
 
-# Central Tendency                                                                                                                                     
+# Central Tendency --------------                                                                                                                                   
 
 def test_mean():
-    pass
+    assert mean([1,2,3,4,5,6,7,8,9,10]) == 5.5
+    assert mean([]) == None
 
 def test_median():
     pass
