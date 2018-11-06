325d3775a164586693b4e2851e9f5e63a7c70ce4
Rezhe <rzwang.ca@gmail.com>
2018-11-06 10:32:05 -0500

    styled code

--- statTools.py
+++ statTools.py
@@ -1,19 +1,23 @@
 import pytest
 from functools import reduce
 
+
 def mean(anyList):
     if anyList != []:
         return reduce(lambda a, b: a + b, anyList) / len(anyList)
     else:
         pass
 
+
 def median(anyList):
     if anyList != []:
         anyList.sort()
         if len(anyList) % 2 != 0:
             return anyList[int(len(anyList) // 2)]
-        else:      
-            return (anyList[int(len(anyList) // 2)] + anyList[int(len(anyList) // 2 - 1)]) /2
+        else:
+            return (
+                anyList[int(len(anyList) // 2)] + anyList[int(len(anyList) // 2 - 1)]
+            ) / 2
     else:
         pass
 
--- test_statTools.py
+++ test_statTools.py
@@ -1,32 +1,45 @@
 import pytest
 from statTools import *
 
-# Central Tendency --------------                                                                                                                                   
+# Central Tendency --------------
+
 
 def test_mean():
-    assert mean([1,2,3,4,5,6,7,8,9,10]) == 5.5
+    assert mean([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 5.5
     assert mean([]) == None
 
+
 def test_median():
-    assert median([2,3,4,5,6,7,8,9,10]) == 6 # ODD
-    assert median([1,2,3,4,5,6,7,8,9,10]) == 5.5 # EVEN
+    assert median([2, 3, 4, 5, 6, 7, 8, 9, 10]) == 6  # ODD
+    assert median([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 5.5  # EVEN
     assert median([]) == None
 
+
 def tests_mode():
-    assert mode([1,2,3,4,4]) == [4]
-    assert mode([1,2,2,3,3]) == [2,3]
-    assert mode([1,2,3,1,2,3]) == [1,2,3]
+    assert mode([1, 2, 3, 4, 4]) == [4]
+    assert mode([1, 2, 2, 3, 3]) == [2, 3]
+    assert mode([1, 2, 3, 1, 2, 3]) == [1, 2, 3]
     assert mode([]) == None
-    
-# Spread                                                                                                                                               
+
+
+# Spread
+
 
 def test_range():
     pass
+
+
 def test_lower_quartile():
     pass
+
+
 def test_upper_quartile():
     pass
+
+
 def test_variance():
     pass
+
+
 def test_standard_deviation():
     pass
