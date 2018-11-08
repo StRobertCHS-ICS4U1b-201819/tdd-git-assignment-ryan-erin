c8605f8f151ac9a961088ef85198de36c0c57f6a
Your Name <erin.chin19@ycdsbk12.ca>
2018-11-07 23:01:20 -0500

    3 variance test cases pass

--- statTools.py
+++ statTools.py
@@ -1,2 +1,22 @@
 def lower_quartile(intList):
     return 2
+
+def upper_quartile():
+    pass
+
+def variance(intList):
+    if len(intList) <= 1:
+        return 0
+    acc = 0
+    variance = 0
+    for i in range(len(intList)):
+        acc += intList[i]
+    acc = acc/len(intList)
+
+    for j in range(len(intList)):
+        variance += (intList[j] - acc) ** 2
+    # round variance to 3 decimal places
+    return round(variance/len(intList), 3)
+
+def standard_deviation():
+    pass
--- test_statTools.py
+++ test_statTools.py
@@ -3,3 +3,12 @@ from statTools import *
 
 def test_lower_quartile_basic1():
     assert(lower_quartile([1, 2, 3, 4, 5]) == 2)
+
+def test_variance_basic1():
+    assert(variance([1, 2, 4, 5]) == 2.500)
+
+def test_variance_basic2():
+    assert(variance([3, 4, 5, 5, 6, 7, 9]) == 3.388)
+
+def test_variance_empty():
+    assert(variance([]) == 0)
