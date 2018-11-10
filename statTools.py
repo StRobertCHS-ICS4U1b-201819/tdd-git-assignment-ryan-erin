aec053bb38ebefa4c786db16bcf7d4ad9e322086
Your Name <erin.chin19@ycdsbk12.ca>
2018-11-09 20:31:03 -0500

    Complete lower quartile function

--- statTools.py
+++ statTools.py
@@ -1,4 +1,23 @@
+def sort(num_list):
+    new_list = []
+    new_list.append(num_list[0])
+    location = 0
+
+    for i in range(1, len(num_list)):
+        # if number is less than the first value in list
+        if num_list[i] < new_list[0]:
+            new_list.insert(0, num_list[i])
+        # else check if it is greater than any of the other values
+        else:
+            # compares number to all the higher values
+            for j in range(len(new_list)):
+                if num_list[i] >= new_list[j]:
+                    location = j + 1
+            new_list.insert(location, num_list[i])
+    return new_list
+
 def lower_quartile(num_list):
+    num_list = sort(num_list)
     first = 0
     last = len(num_list)
     mid = (first + last) // 2
--- test_statTools.py
+++ test_statTools.py
@@ -1,6 +1,18 @@
 import pytest
 from statTools import *
 
+# my own unnecessary sort function tests
+def test_sort_reverse():
+    assert(sort([6, 5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5, 6])
+
+def test_sort_random1():
+    assert(sort([6, 5, 4, 1, 3, 2]) == [1, 2, 3, 4, 5, 6])
+
+def test_sort_random2():
+    assert(sort([7, 45, 4, 1, 8, 13, 9]) == [1, 4, 7, 8, 9, 13, 45])
+
+
+# lower quartile tests
 def test_lower_quartile_basic1():
     assert(lower_quartile([1, 2, 3, 4, 5, 6, 7]) == 2)
 
@@ -13,6 +25,14 @@ def test_lower_quartile_decimals1():
 def test_lower_quartile_decimals2():
     assert(lower_quartile([2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28]) == 8)
 
+def test_lower_quartile_unsorted1():
+    assert(lower_quartile([5, 2, 7, 9, 11, 54, 3]) == 3)
+
+def test_lower_quartile_unsorted2():
+    assert(lower_quartile([5, 2, 38, 7, 9, 11, 54, 3]) == 4)
+
+
+# variance tests
 def test_variance_basic1():
     assert(variance([1, 2, 4, 5]) == 2.500)
 
