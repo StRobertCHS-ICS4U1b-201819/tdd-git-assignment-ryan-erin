81ed35cd86a6a54d528cab625c2922ab23e2fde4
Rezhe <rzwang.ca@gmail.com>
2018-11-11 20:46:32 -0500

    Changes to be committed: 	modified:   statTools.py 	> add function TypeChecker 	> improve mean function 	> and much more ... 	modified:   test_statTools.py 	> add test case for TypeChecker 	> improve test case for all function

--- statTools.py
+++ statTools.py
@@ -1,15 +1,19 @@
 import pytest
 from functools import reduce
 
+typeChecker = lambda anyList: anyList if type(anyList) is list else []
+
 
 def mean(anyList):
+    anyList = typeChecker(anyList)
     if anyList != []:
-        return reduce(lambda a, b: a + b, anyList) / len(anyList)
+        return sum(anyList) / len(anyList)
     else:
         pass
 
 
 def median(anyList):
+    anyList = typeChecker(anyList)
     if anyList != []:
         anyList.sort()
         if len(anyList) % 2 != 0:
@@ -23,6 +27,7 @@ def median(anyList):
 
 
 def mode(anyList):
+    anyList = typeChecker(anyList)
     if anyList != []:
         most = lambda anyList: (max(list(map(anyList.count, anyList))))
         return list(set(filter(lambda x: anyList.count(x) == most(anyList), anyList)))
--- test_statTools.py
+++ test_statTools.py
@@ -1,25 +1,46 @@
-import pytest
 from statTools import *
 
-# Central Tendency --------------
+# Central Tendency
 
+def test_typeChecker():
+    assert typeChecker(12) == [] # int
+    assert typeChecker(1.2) == [] # float
+#   assert typeChecker() == []    # unable with empty values ❌
+    assert typeChecker(None) == [] # none
+    assert typeChecker([]) == [] # empty list
+    assert typeChecker([1,2]) == [1,2] # list
+    assert typeChecker([1,10**1000]) # over max value
 
 def test_mean():
-    assert mean([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 5.5
+    assert mean([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 5.5 # General Cases   
+
+# Corner Cases & Unusual Cases:
     assert mean([]) == None
+    assert mean([1]) == 1
+    assert mean(12) == None 
+    assert mean(None) == None
+#   assert mean([1,10**1000]) # Out of range ❌
 
 
 def test_median():
     assert median([2, 3, 4, 5, 6, 7, 8, 9, 10]) == 6  # ODD
     assert median([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 5.5  # EVEN
+
+# Corner Cases & Unusual Cases:    
     assert median([]) == None
+    assert median("hello") == None
+    assert median(lambda x: [x]) == None
 
 
 def tests_mode():
-    assert mode([1, 2, 3, 4, 4]) == [4]
-    assert mode([1, 2, 2, 3, 3]) == [2, 3]
-    assert mode([1, 2, 3, 1, 2, 3]) == [1, 2, 3]
+    assert mode([1, 2, 3, 4, 4]) == [4] #  1 atom
+    assert mode([1, 2, 2, 3, 3]) == [2, 3] # 2 atom
+    assert mode([1, 2, 3, 1, 2, 3]) == [1, 2, 3] # 3 atom
+
+# Corner Cases & Unusual Cases:
     assert mode([]) == None
+    assert mode(1.5) == None
+    assert mode(map(lambda x: [x],[1])) == None
 
 
 # Spread
@@ -28,18 +49,3 @@ def tests_mode():
 def test_range():
     pass
 
-
-def test_lower_quartile():
-    pass
-
-
-def test_upper_quartile():
-    pass
-
-
-def test_variance():
-    pass
-
-
-def test_standard_deviation():
-    pass
